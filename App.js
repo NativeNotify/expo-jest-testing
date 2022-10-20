44c22d71879f3170448aaa266914125f6e07982c
pagecow <pagecow@gmail.com>
2022-10-20 12:33:29 -0500

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
 
 export default function App() {
+  registerNNPushToken(4450, '9M3rzRSXrOAFOIrBR6gTji');
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
